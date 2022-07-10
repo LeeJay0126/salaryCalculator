dd677d1498d1a87f9b7def051d42ee347f71f706
LeeJay0126 <topaz9889@gmail.com>
2022-07-10 14:31:25 -0700

    Working on display section
    working on JS.

---
+++ salary.js
@@ -0,0 +1,106 @@
+const provinces = ["Alberta","BC", "Saskatchewan","Manitoba", "Ontario"];
+
+const federal = [
+    [50197, 0.15],
+    [50195, 0.205],
+    [55233, 0.26],
+    [66083, 0.29],
+    [0.33]
+]
+
+const federalMax = 0.33;
+
+const Alberta = [
+    [131220, 0.1],
+    [26344, 0.12],
+    [52488, 0.13],
+    [104976, 0.14]
+]
+
+const albertaMax = 0.15;
+
+const BC = [
+    [43070, 0.0506],
+    [43071, 0.077],
+    [12760, 0.105],
+    [21193, 0.1229],
+    [42738, 0.147],
+    [64259, 0.168],
+]
+
+const BCMax = 0.205;
+
+const Saskatchewan = [
+
+    [45677, 0.105],
+    [39152, 0.125],
+
+]
+
+const SaskatchewanMax = 0.145;
+
+const Manitoba = [
+
+    [33723, 0.108],
+    [39162, 0.1275],
+
+]
+
+const ManitobaMax = 0.174;
+
+const Ontario = [
+
+    [45142, 0.0505],
+    [45143, 0.0915],
+    [59712, 0.1116],
+    [70000, 12.16]
+
+]
+
+const OntarioMax = 0.1316;
+
+let provinceSelector = "BC"
+
+function taxBracketInformation(){
+
+    
+
+}
+
+function taxCalculator(income, province, maxProvince){
+
+    let currentBracket = 0;
+    let currentIncome = income;
+    let owedTax = 0;
+
+    while(true) {
+
+        if(currentBracket == province.length){
+            owedTax += currentIncome * maxProvince;
+            break;
+        }
+        
+        if(currentIncome <= province[currentBracket][0]){
+            owedTax += currentIncome * province[currentBracket][1];
+            break;
+        }
+
+        owedTax += province[currentBracket][1] * province[currentBracket][0];
+        currentIncome -= province[currentBracket][0];
+        currentBracket ++;
+
+    }
+
+    return owedTax;
+
+}
+
+function provinceChanger(provinceName){
+
+    if(provinceName != provinceSelector){
+        provinceSelector = provinceName;
+    }
+
+    console.log(provinceSelector);
+
+}
